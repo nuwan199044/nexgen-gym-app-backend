8bb117850346d22db97b41280b1da23e5c9c40f5
Nuwan <nuwan199044@gmail.com>
2024-11-30 03:15:01 +0530

    initial code

---
+++ src/main/java/com/enzith/nexgen/NexgenApplication.java
@@ -0,0 +1,13 @@
+package com.enzith.nexgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexgenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enzith/nexgen/NexgenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enzith.nexgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexgenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
