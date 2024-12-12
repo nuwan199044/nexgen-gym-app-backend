f852e13af8ca4154a4d3f11635afc1de096d4977
Nuwan <nuwan199044@gmail.com>
2024-12-12 09:31:13 +0530

    adding necessary enums.

---
+++ src/main/java/com/enzith/nexgen/enums/PaymentStatus.java
@@ -0,0 +1,5 @@
+package com.enzith.nexgen.enums;
+
+public enum PaymentStatus {
+    PAID, UNPAID
+}
---
+++ src/main/java/com/enzith/nexgen/enums/Status.java
@@ -0,0 +1,5 @@
+package com.enzith.nexgen.enums;
+
+public enum Status {
+    ACTIVE, INACTIVE
+}
