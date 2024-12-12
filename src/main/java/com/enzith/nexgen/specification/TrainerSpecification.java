d52a19f7b6770fd737e9ff03355c0f0ef545e15f
Nuwan <nuwan199044@gmail.com>
2024-12-12 12:28:21 +0530

    adding personal trainer specification.

---
+++ src/main/java/com/enzith/nexgen/specification/TrainerSpecification.java
@@ -0,0 +1,42 @@
+package com.enzith.nexgen.specification;
+
+import com.enzith.nexgen.criteria.TrainerCriteria;
+import com.enzith.nexgen.entity.Trainer;
+import io.micrometer.common.util.StringUtils;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class TrainerSpecification implements Specification<Trainer> {
+
+    private final TrainerCriteria criteria;
+
+    @Override
+    public Predicate toPredicate(Root<Trainer> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+        if (query == null) {
+            throw new IllegalArgumentException("Query cannot be null");
+        }
+        query.distinct(true);
+        List<Predicate> andPredicates = new ArrayList<>();
+
+        if(StringUtils.isNotBlank(criteria.getFirstName())) {
+            Predicate firstNamePredicate = criteriaBuilder.like(root.get("firstName"), "%"+criteria.getFirstName()+"%");
+            andPredicates.add(firstNamePredicate);
+        }
+        if(StringUtils.isNotBlank(criteria.getPhoneNo1())) {
+            Predicate lastNamePredicate = criteriaBuilder.like(root.get("phoneNo1"), "%"+criteria.getPhoneNo1()+"%");
+            andPredicates.add(lastNamePredicate);
+        }
+        if (andPredicates.isEmpty()) {
+            return criteriaBuilder.conjunction();
+        }
+        return criteriaBuilder.or(andPredicates.toArray(new Predicate[0]));
+    }
+}
